1a93cdfbdb6fc1a0cd88c767525817011e9ffa40
weirdG22 <walterssbox@gmail.com>
2024-04-12 23:20:42 -0400

    Add all Module 4 Assignments

---
+++ Module 4/M04-Programming-Assignment/zoo.py
@@ -0,0 +1,10 @@
+# Name: Eli Walters
+# File name: zoo.py
+# Description: Things to Do in chapter 11, thing 1
+
+# 11.1
+def hours():
+    print('Open 9-5 Daily')
+
+if __name__ == "__main__":
+    hours()
