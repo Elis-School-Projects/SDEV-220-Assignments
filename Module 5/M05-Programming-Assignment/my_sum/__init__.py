fe91146f9f6cf825485526ce6b8f0ca5a3c6a021
Eli Walters <ewalters37@ivytech.edu>
2024-04-18 22:33:34 -0400

    Add Module 5 Assignments

---
+++ Module 5/M05-Programming-Assignment/my_sum/__init__.py
@@ -0,0 +1,9 @@
+# Name: Eli Walters
+# File Name: __init__.py
+# Description: Module that provides a basic function to add together an interable list
+
+def sum(arg):
+    total = 0
+    for val in arg:
+        total += val
+    return total
